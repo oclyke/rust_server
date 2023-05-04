ef4268db0a02c13be5f617c00e01e863e0227a99
Owen <o@oclyke.dev>
2023-05-04 13:59:12 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+// note about using rust-analyzer in visual studio code
+// I've disabled default inlay type and parameter info, it is now
+// off unless you press the hotkey combo for inlays ctrl + alt
+// try pressing that (control + option on mac) and see these helpful
+// hints appear! (I turned them off because they clutter a lot and
+// the existing tools [hover, compiler warnings] are enough in most
+// cases for me!)
+// https://stackoverflow.com/questions/69909997/how-can-i-remove-type-annotation-help-when-using-rust-analyzer?rq=1
+
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+use rust_server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::build(4).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
